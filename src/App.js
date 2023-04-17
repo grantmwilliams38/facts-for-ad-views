7268161e1f442bc34b52f0c693ecebfaf239b6e0
grantwilliams <grantmw@uw.edu>
2023-04-16 18:33:46 -0700

    Set up routing with navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import './App.css';
+import React, { useState } from 'react';
+import NavbarWithSearch from './NavbarWithSearch';
+import { Routes, Route } from 'react-router-dom';
+
+const celebrityList = [
+  'Michael Jackson',
+  'Michael Jordan',
+  'Michael Tyson',
+  'Kobe Bryant'
+];
+
+function NotFoundPage(){
+  return (
+    <div>
+      <title>Not Found Page</title>
+      <h1>Not Found Page</h1>
+    </div>
+  )
+}
+
+function HomePage() {
+  return (
+    <div>
+      <title>Home Page</title>
+      <h1>Home Page</h1>
+    </div>
+  )
+}
+
+function Page(celebrity) {
+  return (
+    <div>
+      <title>{celebrity}</title>
+      <h1>{celebrity}</h1>
+    </div>
+  )
+}
 
 function App() {
+  const [activePage, setActivePage] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Blank App</p>
+    <div>
+      <header>
+        <NavbarWithSearch activePage={activePage} setActivePage={setActivePage} celebrityList={celebrityList}/>
       </header>
+      <main className="main">
+        <Routes>
+          <Route index element={HomePage()} />
+          {celebrityList.map((celebrity) => {
+            return <Route key={celebrity} path={"/" + celebrity} element={Page(celebrity)} />
+          })}
+          <Route path="*" element={NotFoundPage()} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/NavbarWithSearch.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useNavigate } from "react-router-dom";
+
+function NavbarWithSearch(props) {
+  const [matchingCelebrities, setMatchingCelebrities] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearchChange = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchQuery(query);
+    const matching = props.celebrityList.filter((celebrity) => {
+      return query !== '' ? celebrity.toLowerCase().startsWith(query) : null;
+    });
+    setMatchingCelebrities(matching);
+  };
+
+  const handleCelebrityClick = (celebrity) => {
+    // Do something with the clicked celebrity (e.g. show their image)
+    navigate(celebrity)
+    setSearchQuery([])
+    setMatchingCelebrities([])
+    console.log(`Clicked on ${celebrity}`);
+    props.setActivePage(celebrity);
+  };
+
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand onClick={() => handleCelebrityClick('')}>Facts for ad views</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <NavDropdown title="All Pages" id="navbarScrollingDropdown">
+                {props.celebrityList.map((celebrity) => {
+                  if (celebrity === props.activePage) {
+                    return (
+                      <NavDropdown.Item key={celebrity} onClick={() => handleCelebrityClick(celebrity)}>
+                        {celebrity}
+                      </NavDropdown.Item>
+                    );
+                  } else {
+                    return (
+                      <NavDropdown.Item key={celebrity} onClick={() => handleCelebrityClick(celebrity)}>
+                        {celebrity}
+                      </NavDropdown.Item>
+                    );
+                  }
+                })}
+              </NavDropdown>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                onChange={handleSearchChange}
+                value={searchQuery}
+              />
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div>
+        <ListGroup>
+          {matchingCelebrities.map((celebrity) => {
+            if (celebrity === props.activePage) {
+              return (
+                <ListGroup.Item key={celebrity} onClick={() => handleCelebrityClick(celebrity)} variant="dark">
+                  {celebrity}
+                </ListGroup.Item>
+              );
+            } else {
+              return (
+                <ListGroup.Item key={celebrity} onClick={() => handleCelebrityClick(celebrity)} variant="secondary">
+                  {celebrity}
+                </ListGroup.Item>
+              );
+            }
+          })}
+        </ListGroup>
+      </div>
+    </div>
+  );
+}
+
+export default NavbarWithSearch;
